4efea17f70aab2ddc489a695fad511e874768db3
Hoang Thong <hoangthongpham@outlook.com>
2018-11-23 17:34:09 +0700

    23.11

--- _pnsdotvn/quontes.php
+++ _pnsdotvn/quontes.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 define('MOD_DIR_UPLOAD', '/media/images/others/');
 define('MOD_ROOT_URL', dirname(dirname(__FILE__)) . '/media/images/others/');
 
@@ -6,15 +8,15 @@ require_once('index_table.php');
 require_once(dirname(dirname(__FILE__)) . '/plugins/upload/class.upload.php');
 
 $titleMenu = 'QUẢN LÝ DANH SÁCH BÁO GIÁ';
-$col = [
-    'id' => 'ID',
-    'full_name' => 'Họ & tên',
-    'email' => 'Email',
-    'phone' => 'Số điện thoại',
+$col       = [
+    'id'         => 'ID',
+    'full_name'  => 'Họ & tên',
+    'email'      => 'Email',
+    'phone'      => 'Số điện thoại',
     'registered' => 'Ngày đăng ký',
-    'qstatus' => 'Trạng thái',
+    'qstatus'    => 'Trạng thái',
 ];
-$iscatURL = (isset($_GET['caturl']) && !empty($_GET['caturl'])) ? $_GET['caturl'] : 0;
+$iscatURL  = (isset($_GET['caturl']) && !empty($_GET['caturl'])) ? $_GET['caturl'] : 0;
 ?>
 
 <?php
@@ -24,7 +26,8 @@ if (isset($_POST['delete']) && !empty($_POST['id'])) {
         $ids = $_POST['id'];
         if (count($ids) > 1) {
             $dbf->deleteData(prefixTable . 'quontes', 'id', $ids);
-        } else {
+        }
+        else {
             $id = end($ids);
             $dbf->deleteData(prefixTable . 'quontes', 'id', $id);
         }
@@ -39,7 +42,7 @@ if (isset($_POST['delete']) && !empty($_POST['id'])) {
 // list quontes
 if (!$isEdit) {
     echo $dbf->returnTitleMenuTable($titleMenu);
-    $url = 'quontes.php?' . (!empty($iscatURL) ? '&caturl=' . $iscatURL : '');
+    $url  = 'quontes.php?' . (!empty($iscatURL) ? '&caturl=' . $iscatURL : '');
     $mang = $dbf->paging(prefixTable . 'quontes', $condition, 'id desc', $url, $PageNo, $PageSize, $Pagenumber, $ModePaging);
     ?>
     <input type="hidden" name="is-quontes" value="true" id="is-quontes">
@@ -60,7 +63,7 @@ if (!$isEdit) {
     $dbf->normalForm('frm', [
         'action' => '',
         'method' => 'post',
-        'class' => 'validate',
+        'class'  => 'validate',
     ])
     ?>
     <div id="panelView" class="panelView">
@@ -68,7 +71,7 @@ if (!$isEdit) {
         $orderBy = [
             [
                 'field' => 'id',
-                'type' => 'DESC',
+                'type'  => 'DESC',
             ],
         ];
         $quontes = $dbf->selectData(prefixTable . 'quontes', [], $orderBy, 20, 0);
@@ -76,7 +79,8 @@ if (!$isEdit) {
             <table id='mainTable' cellpadding='0' cellspacing='0'>
                 <tr>
                     <td class="titleBottom" style="width:7%">
-                        <input type="checkbox" name="chkall" id="chkall" value="1" onclick="docheck(this.checked,0);"
+                        <input type="checkbox" name="chkall" id="chkall"
+                               value="1" onclick="docheck(this.checked,0);"
                                class="checkAll"> √
                     </td>
                     <td class="titleBottom">Họ & tên</td>
@@ -88,7 +92,9 @@ if (!$isEdit) {
                 <?php foreach ($quontes as $quonte): ?>
                     <tr style="cursor:pointer">
                         <td class="cell2">
-                            <input type="checkbox" name="chk" value="<?= $quonte['id'] ?>" onclick="docheckone();"
+                            <input type="checkbox" name="chk"
+                                   value="<?= $quonte['id'] ?>"
+                                   onclick="docheckone();"
                                    class="checkDelete">
                         </td>
                         <td class="cell2">
@@ -142,16 +148,17 @@ if ($isEdit) { ?>
             $conditions = [
                 [
                     'field' => 'id',
-                    'type' => '=',
-                    'value' => @$_GET['edit']
+                    'type'  => '=',
+                    'value' => @$_GET['edit'],
                 ],
             ];
-            $quonte = $dbf->selectData(prefixTable . 'quontes', $conditions, []);
-            $quonte = end($quonte);
+            $quonte     = $dbf->selectData(prefixTable . 'quontes', $conditions, []);
+            $quonte     = end($quonte);
             ?>
             <tr>
                 <td class="boxGrey">Ngày đăng ký</td>
-                <td class="boxGrey2"><strong><?= $quonte['registered'] ?></strong>
+                <td class="boxGrey2">
+                    <strong><?= $quonte['registered'] ?></strong>
                 </td>
             </tr>
             <tr>
@@ -178,35 +185,59 @@ if ($isEdit) { ?>
                         <table id="mainTable" cellpadding="4" cellspacing="0"
                                border="1" style="border-collapse:collapse;">
                             <tr>
-                                <td width="10%" align="center"><strong>Hình ảnh</strong></td>
-                                <td width="45%" align="center"><strong>Sản phẩm</strong></td>
-                                <td width="10%" align="center"><strong>Số lượng</strong></td>
-                                <td width="15%" align="center"><strong>Giá</strong></td>
+                                <td width="10%" align="center"><strong>Hình
+                                        ảnh</strong></td>
+                                <td width="45%" align="center"><strong>Sản
+                                        phẩm</strong></td>
+                                <td width="10%" align="center"><strong>Số
+                                        lượng</strong></td>
+                                <td width="15%" align="center"><strong>Giá bán
+                                        lẻ</strong></td>
+                                <td width="15%" align="center"><strong>Thành
+                                        tiền</strong></td>
                             </tr>
                             <?php foreach ($listIds as $item) {
                                 $conditions = [
                                     [
                                         'field' => 'id',
-                                        'type' => '=',
-                                        'value' => @$item['product_id']
+                                        'type'  => '=',
+                                        'value' => @$item['product_id'],
                                     ],
                                 ];
-                                $product = $dbf->selectData(prefixTable . 'product', $conditions, []);
-                                $product = end($product);
-                                $imgUrl = explode(';', $product['picture']);
-                                $imgUrl = reset($imgUrl); ?>
+                                $product    = $dbf->selectData(prefixTable . 'product', $conditions, []);
+                                $product    = end($product);
+                                $imgUrl     = explode(';', $product['picture']);
+                                $imgUrl     = reset($imgUrl); ?>
                                 <tr>
                                     <td align="center" valign="middle">
-                                        <img src="<?= $imgUrl ?>" alt="<?= $product['name'] ?>" width="25%">
+                                        <img src="<?= $imgUrl ?>"
+                                             alt="<?= $product['name'] ?>"
+                                             width="25%">
                                     </td>
                                     <td style="vertical-align: middle"><?= $product['name'] ?></td>
-                                    <td align="center" style="vertical-align: middle"><?= $item['quantity'] ?></td>
-                                    <td align="center" style="vertical-align: middle">
-                                        <input type="text" name="price[]" value="" size="40">
+                                    <td align="center"
+                                        style="vertical-align: middle"><?= $item['quantity'] ?></td>
+                                    <td align="center"
+                                        style="vertical-align: middle">
+                                        <input type="text" name="price[]"
+                                               value="" size="20"
+                                               data-id="<?php $item['product_id'] ?>">
+                                    </td>
+                                    <td align="center"
+                                        style="vertical-align: middle">
+                                        <input type="text" name="total[]"
+                                               value="" size="20"
+                                               data-id="<?php $item['product_id'] ?>">
                                     </td>
                                 </tr>
                             <?php }
                             ?>
+                            <tr>
+                                <td colspan="5" style="text-align: right">
+                                    <label for="tax"><strong>Thuế</strong></label>
+                                    <input type="text" name="tax" id="tax">
+                                </td>
+                            </tr>
                         </table>
                     <?php } ?></td>
             </tr>
@@ -214,14 +245,22 @@ if ($isEdit) { ?>
                 <td class="boxGrey"></td>
                 <td class="boxGrey2" height="1" align="center">
                     <div id="insert">
-                        <table style="padding-bottom:8px;padding-top:5px;" cellspacing="2" cellpadding="0" border="0">
+                        <table style="padding-bottom:8px;padding-top:5px;"
+                               cellspacing="2" cellpadding="0" border="0">
                             <tbody>
                             <tr>
                                 <td>
-                                    <input class="new-style" name="exportPdf" id="exportPdf" type="button" class="btncenter" value="Xuất PDF" style="cursor: pointer">
+                                    <input class="new-style" name="exportPdf"
+                                           id="exportPdf" type="button"
+                                           class="btncenter" value="Xuất PDF"
+                                           style="cursor: pointer">
                                 </td>
                                 <td>
-                                    <input class="new-style" name="btnhuy" id="btnhuy" type="button" onclick="huy();" class="btncenter" value="Quay lại" style="cursor: pointer">
+                                    <input class="new-style" name="btnhuy"
+                                           id="btnhuy" type="button"
+                                           onclick="huy();" class="btncenter"
+                                           value="Quay lại"
+                                           style="cursor: pointer">
                                 </td>
                             </tr>
                             </tbody>
@@ -232,9 +271,24 @@ if ($isEdit) { ?>
         </table>
     </div>
 <?php } ?>
-<script src="lib/jspdf.debug.js"></script>
-<script>
-    /*var doc = new jsPDF()
-    doc.text('Hello world!', 10, 10)
-    doc.save('a4.pdf')*/
-</script>
+<?php
+require_once 'lib/vendor/autoload.php';
+$mpdf = new \Mpdf\Mpdf([
+    'mode'           => 'utf-8',
+    'tempDir'        => 'tmp',
+    'autoLangToFont' => TRUE,
+    'format'         => 'A4',
+]);
+$mpdf->shrink_tables_to_fit = 1;
+$html = '<h1>heoo</h1>';
+$mpdf->WriteHTML($html);
+$mpdf->SetHTMLFooter('
+            <table width="100%">
+                <tr>
+                    <td align="center">({PAGENO}/{nbpg})</td>
+                </tr>
+            </table>');
+
+ob_clean();
+$mpdf->Output(date('Ymdhis') . '_call_number_.pdf', 'D');
+?>
